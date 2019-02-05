44cfb63ea338a22a67dc6d49796a4217a29441d0
Eric M Goldstein <47017515+EricMGoldstein@users.noreply.github.com>
2019-02-05 15:08:08 -0500

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,9 @@
-placeholder
+<html>
+  <title>  This is my First message into the world since college
+  </title>
+<body>
+    <script language = "javascript">
+      document.write("Hello World! This is my first communicae since college.");
+      document.write("<br>   Trying new things can be fun! </br>");
+</body>
+</html>
