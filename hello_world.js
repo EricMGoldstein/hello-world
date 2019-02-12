410f6e9ec807c4a2ea9d6c17afd0f3416466cbda
Eric M Goldstein <47017515+EricMGoldstein@users.noreply.github.com>
2019-02-12 10:22:58 -0500

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -10,7 +10,7 @@
   <p>this is a line of text for this page <br> this is another line of text
   </p>
 
-<p>Would you like to check out my <a href="https://www.facebook.com/eric.goldstein.10">facebook</a> page?</p>
+<p>Would you like to check out my <a href="https://www.facebook.com/eric.goldstein.10" target="_blank">facebook</a> page?</p>
 
 </body>
 </html>
